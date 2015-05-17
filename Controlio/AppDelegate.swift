4b78cfba524f5258bda70b8a80b2058c42e294e0
backmeupplz <backmeupplz@gmail.com>
2015-05-17 13:24:52 -0700

    Initial commit

---
+++ Controlio/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Controlio
+//
+//  Created by Nikita Kolmogorov on 17/05/15.
+//  Copyright (c) 2015 Borodutch Studio LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+}
+
