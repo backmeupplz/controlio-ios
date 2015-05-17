8266d50d7ff10c926d12ec19248ec0fc24e5627b
backmeupplz <backmeupplz@gmail.com>
2015-05-17 15:51:56 -0700

    add action button to main screen

--- Controlio/ProjectListViewController.swift
+++ Controlio/ProjectListViewController.swift
@@ -9,10 +9,41 @@
 import Foundation
 import UIKit
 
-class ProjectListViewController: UIViewController {
+class ProjectListViewController: UITableViewController {
     
     // MARK: - View Controller Life Cycle -
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.setHidesBackButton(true, animated: false)
+        addRightButton()
+    }
     
+    // MARK: - General Methods -
     
+    func addRightButton() {
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "rightButtonTouched")
+        navigationItem.rightBarButtonItem = refreshButton
+    }
+    
+    func rightButtonTouched() {
+        var alert = UIAlertController(title: "Точно выходим?", message: "Придется опять залогиниться", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Нет", style: .Cancel) { action -> Void in
+            
+        }
+        alert.addAction(cancelAction)
+        
+        let logoutAction: UIAlertAction = UIAlertAction(title: "Пока-пока", style: .Default) { action -> Void in
+            self.logout()
+        }
+        alert.addAction(logoutAction)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func logout() {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
 }
