5760a76bb57aa92981b3e081ac42c503d55808fb
backmeupplz <backmeupplz@gmail.com>
2016-09-21 23:00:57 -0700

    add user cell

---
+++ Controlio/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  Controlio
+//
+//  Created by Nikita Kolmogorov on 21/09/2016.
+//  Copyright © 2016 Nikita Kolmogorov. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
