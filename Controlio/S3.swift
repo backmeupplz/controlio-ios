59dfee798caab2f783ab85fb972e16189da01ee5
backmeupplz <backmeupplz@gmail.com>
2016-10-10 22:35:30 -0700

    fix uploading of multiple images

--- Controlio/S3.swift
+++ Controlio/S3.swift
@@ -65,43 +65,37 @@ class S3: NSObject {
     class func upload(images: [UIImage],
                       progress: @escaping (Float)->(),
                       completion:@escaping ([String]?, String?)->()) {
+        upload(images: images, step: Float(1)/Float(images.count), progressSoFar: 0.0, keys: [], progress: progress, completion: completion)
+    }
+    
+    class func upload(images: [UIImage], step: Float, progressSoFar: Float, keys: [String], progress:@escaping (Float)->(), completion:@escaping ([String]?, String?)->()) {
         if images.count <= 0 {
-            completion([], nil)
+            DispatchQueue.main.async {
+                completion(keys, nil)
+            }
             return
         }
-        var completion: (([String]?, String?)->())? = completion
-        let completedProgress: Float = 1.0
-        let step = completedProgress / Float(images.count)
-        var resultKeys = [String]()
-        var numberOfImagesCompleted = 0
-        var progresses = [UIImage: Float]()
-        for image in images {
-            uploadImage(image, progress:
-            { progressFloat in
-                let relativeProgress = step * progressFloat
-                progresses[image] = relativeProgress
-                var overallProgress: Float = 0.0
-                for (_, prgrs) in progresses {
-                    overallProgress += prgrs
-                }
+        
+        var images = images
+        let firstImage = images.removeFirst()
+        
+        uploadImage(firstImage, progress:
+        { progressFloat in
+            let actualProgress = step * progressFloat
+            DispatchQueue.main.async {
+                progress(progressSoFar + actualProgress)
+            }
+        })
+        { key, error in
+            if let error = error {
                 DispatchQueue.main.async {
-                    progress(overallProgress)
+                    completion(nil, error)
                 }
-            })
-            { key, error in
-                if let error = error {
-                    DispatchQueue.main.async {
-                        completion?(nil, error)
-                    }
-                    completion = nil
-                } else {
-                    numberOfImagesCompleted += 1
-                    resultKeys.append(key!)
-                    if numberOfImagesCompleted >= images.count {
-                        DispatchQueue.main.async {
-                            completion?(resultKeys, nil)
-                        }
-                    }
+            } else {
+                DispatchQueue.main.async {
+                    var keys = keys
+                    keys.append(key!)
+                    upload(images: images, step: step, progressSoFar: progressSoFar+step, keys: keys, progress: progress, completion: completion)
                 }
             }
         }
@@ -110,58 +104,60 @@ class S3: NSObject {
     class func uploadImage(_ image: UIImage,
                            progress: @escaping (_ progress: Float)->(),
                            completion:@escaping (_ key: String?, _ error: String?)->()) {
-        let path = saveImage(image)
-        if path == nil {
-            DispatchQueue.main.async {
-                completion(nil, "Could not save image to file system")
-            }
-            return
-        }
-        
-        let ext = "png"
-        let uploadRequest = AWSS3TransferManagerUploadRequest()
-        uploadRequest?.body = URL(fileURLWithPath: path!)
-        let userId = Server.currentUser?.id ?? "no_userId"
-        uploadRequest?.key = "\(userId)/" + ProcessInfo.processInfo.globallyUniqueString + "-\(Int(Date().timeIntervalSince1970))" + "." + ext
-        uploadRequest?.bucket = S3BucketName
-        uploadRequest?.uploadProgress = { bytesSent, totalBytesSent, totalBytesExpectedToSend in
-            DispatchQueue.main.async {
-                progress(Float(totalBytesSent) / Float(totalBytesExpectedToSend))
-            }
-        }
-        
-        let transferManager = AWSS3TransferManager.default()
-        transferManager?.upload(uploadRequest).continue({ (task) -> AnyObject! in
-            if let error = task.error {
+        DispatchQueue.global().async {
+            let path = saveImage(image)
+            if path == nil {
                 DispatchQueue.main.async {
-                    completion(nil,
-                               error.localizedDescription)
+                    completion(nil, "Could not save image to file system")
                 }
-            } else if let exception = task.exception {
-                
-                completion(nil,
-                           exception.name.rawValue)
-            } else if task.result != nil {
-                // Success
+                return
+            }
+            
+            let ext = "png"
+            let uploadRequest = AWSS3TransferManagerUploadRequest()
+            uploadRequest?.body = URL(fileURLWithPath: path!)
+            let userId = Server.currentUser?.id ?? "no_userId"
+            uploadRequest?.key = "\(userId)/" + ProcessInfo.processInfo.globallyUniqueString + "-\(Int(Date().timeIntervalSince1970))" + "." + ext
+            uploadRequest?.bucket = S3BucketName
+            uploadRequest?.uploadProgress = { bytesSent, totalBytesSent, totalBytesExpectedToSend in
                 DispatchQueue.main.async {
-                    completion(uploadRequest!.key!, nil)
-                }
-            } else {
-                DispatchQueue.main.async {
-                    completion(nil, "Unexpected empty result")
+                    progress(Float(totalBytesSent) / Float(totalBytesExpectedToSend))
                 }
             }
             
-            // Remove file
-            let fileManager = FileManager.default
-            do {
-                try fileManager.removeItem(atPath: path!)
-            }
-            catch let error as NSError {
-                print("File not deelted: \(error)")
-            }
-            return nil
-        })
+            let transferManager = AWSS3TransferManager.default()
+            transferManager?.upload(uploadRequest).continue({ (task) -> AnyObject! in
+                if let error = task.error {
+                    DispatchQueue.main.async {
+                        completion(nil,
+                                   error.localizedDescription)
+                    }
+                } else if let exception = task.exception {
+                    
+                    completion(nil,
+                               exception.name.rawValue)
+                } else if task.result != nil {
+                    // Success
+                    DispatchQueue.main.async {
+                        completion(uploadRequest!.key!, nil)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(nil, "Unexpected empty result")
+                    }
+                }
+                
+                // Remove file
+                let fileManager = FileManager.default
+                do {
+                    try fileManager.removeItem(atPath: path!)
+                }
+                catch let error as NSError {
+                    print("File not deelted: \(error)")
+                }
+                return nil
+            })
+        }
     }
 
     // MARK: - Private class functions -
