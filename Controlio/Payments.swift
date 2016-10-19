8b0bd9fd63cfa5fbc985c643f851f67ae4cc34b0
backmeupplz <backmeupplz@gmail.com>
2016-10-19 12:42:06 -0700

    start messing with cocoapods to figure out what's wrong with stripe

---
+++ Controlio/Payments.swift
@@ -0,0 +1,13 @@
+//
+//  Payments.swift
+//  Controlio
+//
+//  Created by Nikita Kolmogorov on 18/10/2016.
+//  Copyright © 2016 Nikita Kolmogorov. All rights reserved.
+//
+
+import UIKit
+
+class Payments: NSObject {
+
+}
